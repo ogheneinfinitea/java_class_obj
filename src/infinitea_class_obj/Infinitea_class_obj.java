72de9daf619800f7dd97fdecb1827efe9574847c
Eduotu Ogheneriye Samson <ogheneinfinitea@gmail.com>
2019-07-02 00:48:42 +0100

    values assigned to the instance

--- src/infinitea_class_obj/Infinitea_class_obj.java
+++ src/infinitea_class_obj/Infinitea_class_obj.java
@@ -11,18 +11,25 @@ package infinitea_class_obj;
  */
 public class Infinitea_class_obj {
 
-    public class Customter {
+   
         //declarig instance of a variable
        int customerID;
        String customerName;
        String customerAddress;
        int customerAge;
-    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Infinitea_class_obj objCustomer1 = new Infinitea_class_obj();
+        objCustomer1.customerID=100;
+        objCustomer1.customerAge=21;
+        objCustomer1.customerID=103;
+        objCustomer1.customerName="SAMSON";
+        
+        System.out.println("Customer Identification Number:");
     }
     
 }
