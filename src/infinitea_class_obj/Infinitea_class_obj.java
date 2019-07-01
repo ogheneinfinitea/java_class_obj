bb515746734e8fdc360025c04723b75543c8d5bc
Eduotu Ogheneriye Samson <ogheneinfinitea@gmail.com>
2019-07-02 00:20:27 +0100

    new project for claases and objects in java

---
+++ src/infinitea_class_obj/Infinitea_class_obj.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infinitea_class_obj;
+
+/**
+ *
+ * @author Rejoice Ihuoma
+ */
+public class Infinitea_class_obj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
