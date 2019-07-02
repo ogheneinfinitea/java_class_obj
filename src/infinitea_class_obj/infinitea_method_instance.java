4c71b4abcf5335b2d734d6837407c75d8e8a61e7
Eduotu Ogheneriye Samson <ogheneinfinitea@gmail.com>
2019-07-02 01:04:51 +0100

    implemetation of method instance

---
+++ src/infinitea_class_obj/infinitea_method_instance.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infinitea_class_obj;
+
+/**
+ *
+ * @author Rejoice Ihuoma
+ */
+public class infinitea_method_instance {
+ 
+    
+    public static void main(String[] args) {
+}
+}
